11d5cee14c1a60cc714cbf76a9ca66c83380b8a9
“presly2024” <preslytakopnfor@gmail.com>
2024-12-15 06:17:01 +0100

    end point update 2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
      import.meta.env.VITE_MODE === "development"
           ? "http://localhost:4000"
-          : "https://note-api-azure.vercel.app/";
+          : "https://note-api-tau.vercel.app/";
